5df22e7ee61486838aaa792cb3706a7c596626b9
Pravin <iampskathane@gmail.com>
2025-10-28 17:51:05 +0530

    Comments and Shortcuts

---
+++ Excercises/Comments_and_shortcuts.py
@@ -0,0 +1,44 @@
+#  This is a single line command
+x = 10  # Assign the value of 10 to var x
+y = 20  # Assign the value of 20 to var y
+
+#calculate the sum of x + y
+
+result =  x + y
+
+print(result)
+
+# This is multi-line comment
+# created by using multiple
+# single-line comments.
+
+"""
+This is multi-line comment
+created by using string literals uding double quotes.
+"""
+
+'''
+This is multi-line comment
+created by using string literals using single quotes.
+'''
+
+# commented out code
+# x = 10  # Assign the value of 10 to var x
+# y = 20  # Assign the value of 20 to var y
+#
+# #calculate the sum of x + y
+#
+# result =  x + y
+#
+# print(result)
+
+# Pycharm shortcuts
+# CTRL + / -
+# To comment the code select the lines of code and press CTRL + /
+# for commenting out code, again select the lines and press CTRL + /
+
+# Shift + Alt + . - to make font size larger
+# Shift + Alt + , - to make font size smaller
+
+# Ctrl + d -  to duplicate line
+
