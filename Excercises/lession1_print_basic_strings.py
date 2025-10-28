5e5dc6bedf54e067e1faf364208e46ce057ff08c
Pravin <iampskathane@gmail.com>
2025-10-28 16:29:21 +0530

    Lession for print and exercise

---
+++ Excercises/lession1_print_basic_strings.py
@@ -0,0 +1,26 @@
+# 1. Print a short story using multiple lines
+# TODO:
+print("Hello, World!!")
+print("I'm learning Python!")
+print("This is so much fun!")
+print("I can't wait to learn more!")
+
+print("---------------------------------------")
+print("---------------------------------------")
+# 2. Create string that includes both single and double quotes
+# TODO:
+print("He said, 'Python is awesome!'")
+print('She replied, "I totally agree!"')
+print("---------------------------------------")
+print("---------------------------------------")
+# 3. Use escape chatracters to create formatted list.
+# TODO:
+print("First line \nSecond line")
+print("This\tis\ttabbed")
+print("---------------------------------------")
+print("---------------------------------------")
+# 4. Combine different types of data(Strings and numbers in one string)
+# TODO:
+print("My name is", "Pravin", "and I am", 33, "years old.")
+print("---------------------------------------")
+print("---------------------------------------")
