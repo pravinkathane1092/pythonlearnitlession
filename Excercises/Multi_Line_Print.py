b631e98ac31ac9264b75814bd4548db62507ddb8
Pravin <iampskathane@gmail.com>
2025-10-28 17:02:18 +0530

    multi_line_print_staement

---
+++ Excercises/Multi_Line_Print.py
@@ -0,0 +1,16 @@
+
+
+print("Using multiple print statements")
+print("Roses are red,")
+print("Violets are blue,")
+print("python is awesome, and so are you!")
+
+print("\n")
+print("Using single print statements")
+print("Roses are red,\nViolets are blue,\npython is awesome, and so are you!")
+
+print("\n")
+print("Using triple quotes ")
+print('''Roses are red,
+Violets are blue,
+python is awesome, and so are you!''')
